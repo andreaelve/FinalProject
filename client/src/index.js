dfe4e67b52e90497100b10139186e89a5103ba2b
nfluk <nickylukic@gmail.com>
2022-07-21 08:32:39 +0200

    thursday start

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import { Auth0Provider } from "@auth0/auth0-react";
 import React, { createContext, useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 import express from 'express';
 const PORT = process.env.PORT || 3001;
 const app = express();
-import cors from 'cors';
-import fetch from'node-fetch';
 import { MongoClient } from 'mongodb';
 import bodyParser from 'body-parser';
 
@@ -12,41 +10,34 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 const PASSWORD = '1234SALT';
+const uri = 'mongodb+srv://codeClub:'+PASSWORD+'@movie-project.rhbq4r1.mongodb.net/?retryWrites=true&w=majority';
 
-// app.get("/api", (req, res) => {
-//   res.json({ message: "Hello from Express!" });
-// });
+const createListingForNewUser = async (userInfo) => {
+  const client = new MongoClient(uri);
+    try {
+        await client.connect();
+        await createListing(client,
+          {
+            user: userInfo.email,
+            liked_movies: [
+            ],
+            disliked_movies: [
+            ]
+          }
+        )
+    } finally {
+        await client.close();
+    }
+}
 
-
-// app.get("/search/:search", (req,res)=>{
-//   const word =  req.params.search;
-//   client.define(word)
-  
-//   .then(function(result){
-      
-//      return res.status(200).json(result).end();
-//    });
-   
-//        });
-app.use(cors())
-
-app.get('/movie', async (req,res)=>{
-  const url = "https://api.themoviedb.org/3/discover/movie?with_genres=18&api_key=2b61576c6129138ce5beeb3937518565&language=en-US";
-  const option= {
-    "method" : "GET",
-  }
-  const response = await fetch(url, option)
-  .then(res => res.json())
-  .catch(e => {
-    console.log({
-      "message" : "oh noes",
-      error : e,
-    });
+app.post('/register', async (req, res) => {
+  // todo: validate input
+  console.log('hhhhhh', req.body)
+  await createListingForNewUser(req.body);
+  res.json({
+    message: 'Yayy!'
   })
-
-  console.log("RESPONSE", response);
-  res.json(response)
-})
+});
 
 // Get for the initial setup when a user logs in. 
 
@@ -56,7 +47,7 @@ app.get('/movie', async (req,res)=>{
 // There's a problem with the database. Maybe because the user does not exist in the database yet.
 app.post('/movie', async (req, res) => {
   console.log('heisann', req.body);
-  const uri = 'mongodb+srv://codeClub:'+PASSWORD+'@movie-project.rhbq4r1.mongodb.net/?retryWrites=true&w=majority';
+  // const uri = 'mongodb+srv://codeClub:'+PASSWORD+'@movie-project.rhbq4r1.mongodb.net/?retryWrites=true&w=majority';
   console.log('yo')
   MongoClient.connect(uri, function(err, db) {
     if (err) throw err;
@@ -72,24 +63,6 @@ app.post('/movie', async (req, res) => {
     });
   });
 })
-
-// app.post('/movie', async (req, res) => {
-//   console.log('heisann', req.body);
-//   const uri = 'mongodb+srv://codeClub:'+PASSWORD+'@movie-project.rhbq4r1.mongodb.net/?retryWrites=true&w=majority';
-//   console.log('yo')
-//   MongoClient.connect(uri, function(err, db) {
-//     if (err) throw err;
-//     const dbo = db.db("movies_db");
-//     const myquery = { user: req.body.user };
-//     const newvalues = { $set: { disliked_movies: req.body.disliked_movies } };
-//     dbo.collection("movie_collection").updateOne(myquery, newvalues, function(err, res) {
-//       if (err) throw err;
-//       console.log("1 document updated");
-//       db.close();
-//       console.log('dislikeeeeeee', newvalues);
-//     });
-//   });
-// })
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
