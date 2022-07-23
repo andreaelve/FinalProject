bf3d6ee89b3661516d188cc096a0abc58f5daacc
nfluk <nickylukic@gmail.com>
2022-07-23 15:31:51 +0200

    branch check

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,9 @@
-import "../App.css"
-import { useAuth0 } from "@auth0/auth0-react";
+import '../App.css';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const Login = () => {
   const { loginWithRedirect } = useAuth0();
-  
+
   return (
     <div className="login">
       <h1>Login</h1>
