be4bb4d5cf908333eeacd2aab504db37ec7cff0b
Andrea Elvegård <monikavalkova10@gmail.com>
2022-07-25 16:23:44 +0200

    please work

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '../client', 'build/index.html'));
 });
 
